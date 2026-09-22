5fbe9fa212e66e05c27fd4770bf8406c1ea44645
Alan Silva <alanclash2541@gmail.com>
2026-09-22 00:44:51 -0300

    Add validation for registered date in PatientRequestDTO to ensure it is not in the future

--- patient-service/src/main/java/dev/bug/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/dev/bug/patientservice/dto/PatientRequestDTO.java
@@ -3,6 +3,7 @@ package dev.bug.patientservice.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -22,6 +23,7 @@ public record PatientRequestDTO(
         @NotNull(message = "Date of birth is required")
         LocalDate dateOfBirth,
 
+        @PastOrPresent(message = "Registered date cannot be in the future")
         @NotNull(message = "Registered date is required")
         LocalDate registeredDate
 ) {
